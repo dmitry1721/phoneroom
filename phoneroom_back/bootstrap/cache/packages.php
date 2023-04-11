56ce57da45fa7488b728d49b37cbbad6fb97b684
dmitry1721 <76875092+dmitry1721@users.noreply.github.com>
2023-04-11 15:26:24 +0300

    Add files via upload

---
+++ phoneroom_back/bootstrap/cache/packages.php
@@ -0,0 +1,140 @@
+<?php return array (
+  'barryvdh/laravel-debugbar' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\Debugbar\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Debugbar' => 'Barryvdh\\Debugbar\\Facades\\Debugbar',
+    ),
+  ),
+  'laravel-notification-channels/telegram' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NotificationChannels\\Telegram\\TelegramServiceProvider',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/scout' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Scout\\ScoutServiceProvider',
+    ),
+  ),
+  'laravel/telescope' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Telescope\\TelescopeServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravel/ui' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Ui\\UiServiceProvider',
+    ),
+  ),
+  'lee-to/laravel-phone-auth' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Leeto\\PhoneAuth\\Providers\\PhoneAuthServiceProvider',
+    ),
+  ),
+  'livewire/livewire' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Livewire\\LivewireServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Livewire' => 'Livewire\\Livewire',
+    ),
+  ),
+  'movemoveapp/laravel-dadata' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'MoveMoveIo\\DaData\\DaDataServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'DaDataAddress' => 'MoveMoveIo\\DaData\\Facades\\DaDataAddress',
+      'DaDataName' => 'MoveMoveIo\\DaData\\Facades\\DaDataName',
+      'DaDataEmail' => 'MoveMoveIo\\DaData\\Facades\\DaDataEmail',
+      'DaDataPhone' => 'MoveMoveIo\\DaData\\Facades\\DaDataPhone',
+      'DaDataCompany' => 'MoveMoveIo\\DaData\\Facades\\DaDataCompany',
+      'DaDataBank' => 'MoveMoveIo\\DaData\\Facades\\DaDataBank',
+      'DaDataPassport' => 'MoveMoveIo\\DaData\\Facades\\DaDataPassport',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'spatie/laravel-ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Spatie\\LaravelIgnition\\Facades\\Flare',
+    ),
+  ),
+  'stevebauman/location' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Stevebauman\\Location\\LocationServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Location' => 'Stevebauman\\Location\\Facades\\Location',
+    ),
+  ),
+);
